1dd6e32b7d0931f10d7718837d962ac05b3c4efa
EmanFouad <engemanf11@gmail.com>
2020-03-11 20:33:38 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    'use strict';
+
+//portfolio shuffle
+$('.portfolio ul li').on('click', function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    if($(this).data('class') === 'all'){
+        $('.shuffle-images .col-md').css('opacity','1');
+    }else{
+        $('.shuffle-images .col-md').css('opacity' , '.08');
+        $($(this).data('class')).parent().css('opacity' , '1');
+    }
+})
+
+});
